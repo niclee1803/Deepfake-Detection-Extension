d68c75f2c74db87dd70236eb85f1cd120ce35b81
niclee1803 <niclee1803@outlook.com>
2025-03-09 01:00:33 +0800

    deepfake detector v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from pretrainedmodels import xception
-from transformers import AutoImageProcessor, SiglipForImageClassification
+from transformers import AutoImageProcessor, SiglipForImageClassification, ViTForImageClassification, ViTImageProcessor
 from PIL import Image
 import io
 
@@ -46,6 +46,9 @@ xception_transform = transforms.Compose([
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
 
+# Load Deep-Fake-Detector-v2-Model
+model = ViTForImageClassification.from_pretrained("prithivMLmods/Deep-Fake-Detector-v2-Model")
+processor = ViTImageProcessor.from_pretrained("prithivMLmods/Deep-Fake-Detector-v2-Model")
 
 # SigLIP classifier
 def classify_with_siglip(image_data):
@@ -64,6 +67,7 @@ def classify_with_siglip(image_data):
 
 # Xception classifier
 def classify_with_xception(image_data):
+    print("lmaop")
     image = Image.open(io.BytesIO(image_data)).convert("RGB")
     image = xception_transform(image).unsqueeze(0)
 
@@ -78,6 +82,25 @@ def classify_with_xception(image_data):
     return result
 
 
+# Deep-Fake-Detector-v2-Model
+def classify_with_deepfakedetectorv2(image_data):
+    print("DEBUG: Calling classify_with_deepfakedetectorv2()...")
+
+    # Load and preprocess the image
+    image = Image.open(io.BytesIO(image_data)).convert("RGB")
+    inputs = processor(images=image, return_tensors="pt")
+
+    # Perform inference
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+        predicted_class = torch.argmax(logits, dim=1).item()
+
+    # Map class index to label
+    label = model.config.id2label[predicted_class]
+    return label
+
+
 @app.post("/detect/")
 async def detect_deepfake(file: UploadFile = File(...)):
     image_data = await file.read()
@@ -85,10 +108,12 @@ async def detect_deepfake(file: UploadFile = File(...)):
 
     siglip_result = classify_with_siglip(image_data)
     xception_result = classify_with_xception(image_data)
+    deepfakeDetectorV2_result = classify_with_deepfakedetectorv2(image_data)
 
     combined_result = {
         "SigLIP": siglip_result,
-        "XceptionNet": xception_result
+        "XceptionNet": xception_result,
+        "DeepfakeDetectorV2": deepfakeDetectorV2_result
     }
 
     print(combined_result)
